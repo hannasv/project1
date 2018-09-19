ddf9257c0ec12126c755672f9d1ab9f02beb770e
Hanna Svennevik <svennevikh@gmail.com>
2018-09-19 17:54:44 +0200

    moved designmatrix to model_comparison

--- model_comparison.py
+++ model_comparison.py
@@ -20,12 +20,30 @@ from model_selection import GridSearch
 # Google gridsearch sklearn
     # finne lmd verdien som er best for hvert dataset.
 
-def bootstrap(X, random_state):
-        bootVec = np.random.choice(X, len(X))
-        return bootVec,X
+def bootstrap(x, random_state):
+        bootVec = np.random.choice(x, len(x))
+        return bootVec,x # returns x_train, x_test
+
+def generateDesignmatrix(p, x, y):
+    m = int((p**2+3*p+2)/2) # returnerer heltall for p = [1:5]
+    X = np.zeros((len(x), m))
+    X[:,0] = 1
+    counter = 1
+    for i in range(1, p+1):
+        for j in range(i+1):
+            X[:,counter] = x**(i-j) * y**j
+            counter+=1
+    return X
+
+def frankeFunction(x,y):
+    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+    return term1 + term2 + term3 + term4
 
 
-def model_comparison(models, param_grid, X, y, random_states):
+def model_comparison(models, param_grid, p, x, y, random_states):
     """Perform the model comparison experiment.
 
     Args:
@@ -37,7 +55,6 @@ def model_comparison(models, param_grid, X, y, random_states):
 
     Returns:
         (dict): Model scores.
-
     """
 
     train_scores, test_scores = {}, {}
@@ -46,17 +63,23 @@ def model_comparison(models, param_grid, X, y, random_states):
         train_scores[name], test_scores[name] = [], []
         for random_state in random_states:
 
-# random states ?? bytt seed
-
             # Generate data (bootstrap sampling in your case).
-            X_train, X_test = bootstrap(X, random_state=random_state)
+            print(x.shape)
+            x_train, x_test = bootstrap(x, random_state=random_state)
             y_train, y_test = bootstrap(y, random_state=random_state)
+
+            X_train = generateDesignmatrix(p, x_train, y_train)
+            X_test = generateDesignmatrix(p, x_test, y_test)
+
+            z_train = frankeFunction(x_train, y_train)
+            z_test = frankeFunction(x_test, y_test)
 
             # Pass algorithm + corresponding params to grid searcher and
             # determine optimal alpha param.
             grid = GridSearch(estimator, param_grid[name], random_state)
-            #grid.fit(X, y)
-            grid.fit(X_train, X_test, y_train, y_test)
+
+
+            grid.fit(X_train, X_test, z_train, z_test)
 
             # Store svg score values for each model.
             train_scores[name].append(np.mean(grid.train_scores))
