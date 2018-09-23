d7733bd52d9d9fd0dc6b15aa9dee3dedbfa7b616
linapau <paulinatedesco@gmail.com>
2018-09-24 01:59:41 +0200

    Added bias calculation and removed some comments
    Check calculation of bias.
    
    Also removed some comments.
    
    TODO: calculate covariance

--- model_comparison_new.py
+++ model_comparison_new.py
@@ -17,79 +17,6 @@ import algorithms
 import numpy as np
 from model_selection_new import GridSearchNew
 
-# Google gridsearch sklearn
-# finne lmd verdien som er best for hvert dataset.
-
-# def generateDesignmatrix(p, x, y):
-#     m = int((p**2+3*p+2)/2)  # returnerer heltall for p = [1:5]
-#     X = np.zeros((len(x), m))
-#     X[:, 0] = 1
-#     counter = 1
-#     for i in range(1, p+1):
-#         for j in range(i+1):
-#             X[:, counter] = x**(i-j) * y**j
-#             counter += 1
-#     return X
-#
-#
-# def frankeFunction(x, y):
-#     term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
-#     term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
-#     term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
-#     term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
-#     return term1 + term2 + term3 + term4
-#
-#
-# def bootstrap(X, z, random_state):
-#
-#     # For random randint
-#     np.random.seed(random_state)
-#
-#     nrows, ncols = np.shape(X)
-#
-#     selected_rows = np.random.randint(
-#         low=0, high=nrows, size=(nrows, ncols)
-#     )
-#     selected_cols = np.random.randint(
-#         low=0, high=ncols, size=(nrows, ncols)
-#     )
-#     X_subset = X[selected_rows, selected_cols]
-#     z_subset = z[selected_rows]
-#
-#     return X_subset, z_subset
-#
-#
-# def train_test_split(X, z, split_size=0.2, random_state=None):
-#
-#     # For random choice.
-#     np.random.seed(random_state)
-#
-#     # Extract number of rows and columns in data matrix.
-#     nrows, ncols = np.shape(X)
-#
-#     # Determine the proportion of training and test samples
-#     # from the data matrix size-
-#     ntest_samples = int(nrows * split_size)
-#     ntrain_samples = int(nrows - ntest_samples)
-#     # Randomly select indices for training and test samples
-#     # without replacement.
-#     row_samples = np.arange(nrows)
-#     selected_train_samples = np.random.choice(
-#         row_samples, ntrain_samples, replace=False
-#     )
-#     selected_test_samples = [
-#         sample for sample in row_samples
-#         if sample not in selected_train_samples
-#     ]
-#     # Extract training and test samples based on selected
-#     # indices.
-#     X_train = X[selected_train_samples, :]
-#     X_test = X[selected_test_samples, :]
-#     z_train = z[selected_train_samples]
-#     z_test = z[selected_test_samples]
-#
-#     return X_train, X_test, z_train, z_test
-
 
 def model_comparison_new(models, param_grid, X, z, split_size=0.2, verbose=True):
     """Perform the model comparison experiment.
@@ -126,15 +53,12 @@ def model_comparison_new(models, param_grid, X, z, split_size=0.2, verbose=True)
         mse_all_models.append(grid.mse)
         r2_all_models.append(grid.r2)
 
-        # # Store average score values (over all lambdas) for each model.
-        # avg_train_scores_mse.append(np.mean(grid.train_scores_mse)) # we should save the avg over all boots for the best lambda !!!
-        # avg_test_scores_mse.append(np.mean(grid.test_scores_mse))
-        #
-        # avg_train_scores_r2.append(np.mean(grid.train_scores_r2))
-        # avg_test_scores_r2.append(np.mean(grid.test_scores_r2))
+        # Bias of best lambda calculated for each model ---> check maths!!
+        best_bias_mse = (np.mean(z) - grid.best_avg_z_pred_mse)**2
+        best_bias_r2 = (np.mean(z) - grid.best_avg_z_pred_mse) ** 2
 
-
-
+        # TODO: calculate the bias for all the lambdas
+        # calculate covariance
 
         if verbose:
             print('Best mse score: {}'.format(grid.best_mse),
@@ -143,8 +67,12 @@ def model_comparison_new(models, param_grid, X, z, split_size=0.2, verbose=True)
                   'Best lambda: {}'.format(grid.best_param_r2))
 
         results[name] = {
-            'mse': grid.mse,
-            'r2': grid.r2,
+            'Best mse': grid.best_mse,
+            'Best mse lambda': grid.best_param_mse,
+            'Best r2': grid.best_r2,
+            'Best r2 lambda': grid.best_param_r2,
+            'Bias for best MSE': best_bias_mse,
+            'Bias for best r2': best_bias_r2
         }
 
     return results
