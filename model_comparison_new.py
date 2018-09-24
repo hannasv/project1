Corrected z_pred and added model variance
The average calculation in each boot of z_pred was missing, causing wrong dimensions. This problem is now solved.

I also calculated the variance: best_var_mse, and best_var_r2.

This version runs.

There is an error in master, so I did not merge this version

model_comparison_new.py
@@ -57,6 +57,18 @@ def model_comparison_new(models, param_grid, X, z, split_size=0.2, verbose=True)
         best_bias_mse = (np.mean(z) - grid.best_avg_z_pred_mse)**2
         best_bias_r2 = (np.mean(z) - grid.best_avg_z_pred_mse) ** 2
 
+        # # Variance of best lambda calculated for each model ---> check maths!!
+        nrep = len(grid.best_z_pred_mse)
+        mean_z_rep = np.squeeze((np.repeat(np.mean(z), nrep)))
+        best_var_mse = (np.sum((mean_z_rep - grid.best_z_pred_mse)**2))/grid.nboots
+        best_var_r2 = (np.sum((mean_z_rep - grid.best_z_pred_r2)**2))/grid.nboots
+        #print(len(grid.best_z_pred_r2))
+
+        #print('X_test', grid.test.shape) # test dimensions
+
+
+
+
         # TODO: calculate the bias for all the lambdas
         # calculate covariance
 
@@ -72,7 +84,9 @@ def model_comparison_new(models, param_grid, X, z, split_size=0.2, verbose=True)
             'Best r2': grid.best_r2,
             'Best r2 lambda': grid.best_param_r2,
             'Bias for best MSE': best_bias_mse,
-            'Bias for best r2': best_bias_r2
+            'Bias for best r2': best_bias_r2,
+            'Variance for best MSE': best_var_mse,
+            'Variance for best r2': best_var_r2
         }
 
     return results