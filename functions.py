dbc7cdbde594e1abd651fe79cab60846f77457f6
linapau <paulinatedesco@gmail.com>
2018-09-25 11:49:06 +0200

    try to fix merge

---
+++ functions.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+def generateDesignmatrix(p, x, y):
+    m = int((p**2+3*p+2)/2)  # returnerer heltall for p = [1:5]
+    X = np.zeros((len(x), m))
+    X[:, 0] = 1
+    counter = 1
+    for i in range(1, p+1):
+        for j in range(i+1):
+            X[:, counter] = x**(i-j) * y**j
+            counter += 1
+    return X
+
+
+def frankeFunction(x, y):
+    term1 = 0.75*np.exp(-(0.25*(9*x-2)**2) - 0.25*((9*y-2)**2))
+    term2 = 0.75*np.exp(-((9*x+1)**2)/49.0 - 0.1*(9*y+1))
+    term3 = 0.5*np.exp(-(9*x-7)**2/4.0 - 0.25*((9*y-3)**2))
+    term4 = -0.2*np.exp(-(9*x-4)**2 - (9*y-7)**2)
+    return term1 + term2 + term3 + term4
+
+
+def bootstrap(X, z, random_state):
+
+    # For random randint
+    np.random.seed(random_state)
+
+    nrows, ncols = np.shape(X)
+
+    selected_rows = np.random.randint(
+        low=0, high=nrows, size=(nrows, ncols)
+    )
+    selected_cols = np.random.randint(
+        low=0, high=ncols, size=(nrows, ncols)
+    )
+    X_subset = X[selected_rows, selected_cols]
+    z_subset = z[selected_rows]
+
+    return X_subset, z_subset
+
+
+def train_test_split(X, z, split_size=0.2, random_state=None):
+
+    # For random choice.
+    np.random.seed(random_state)
+
+    # Extract number of rows and columns in data matrix.
+    nrows, ncols = np.shape(X)
+
+    # Determine the proportion of training and test samples
+    # from the data matrix size-
+    ntest_samples = int(nrows * split_size)
+    ntrain_samples = int(nrows - ntest_samples)
+    # Randomly select indices for training and test samples
+    # without replacement.
+    row_samples = np.arange(nrows)
+    selected_train_samples = np.random.choice(
+        row_samples, ntrain_samples, replace=False
+    )
+    selected_test_samples = [
+        sample for sample in row_samples
+        if sample not in selected_train_samples
+    ]
+    # Extract training and test samples based on selected
+    # indices.
+    X_train = X[selected_train_samples, :]
+    X_test = X[selected_test_samples, :]
+    z_train = z[selected_train_samples]
+    z_test = z[selected_test_samples]
+
+    return X_train, X_test, z_train, z_test
