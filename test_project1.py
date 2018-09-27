c0de029a1278e591ae2d03650c49bc54238c9515
Hanna Svennevik <svennevikh@gmail.com>
2018-09-27 17:28:13 +0200

    BM: working version pytest

--- test_project1.py
+++ test_project1.py
@@ -10,7 +10,7 @@ from sklearn.pipeline import make_pipeline
 import algorithms
 import model_selection_new
 from model_comparison_new import model_comparison_new
-from functions import generateDesignmatrix, franke_function
+from functions import generateDesignmatrix, franke_function, train_test_split
 
 np.random.seed(1000)
 m = 30  # m defines the size of the meshgrid.
@@ -18,30 +18,38 @@ x = np.random.rand(m, )
 y = np.random.rand(m, )
 z = franke_function(x, y)
 
-def test_design(p=1):
-    if (p==1):
-        X = [np.ones(len(x)), x, y]
-        #for i in range(len(x)):
-        #    new[:, i] = X[:i]
-        assert np.all(abs(X- generateDesignmatrix(p,x,y))<1e8)
-        #assert np.allclose(X, generateDesignmatrix(p,x,y))
+def convert(X):
+    m = len(X)
+    n = len(X[0])
+    new = np.zeros((n,m))
+    for i in range(m): #3
+        for j in range(n): # 30
+            new[j][i] = X[i][j]
+    return new
 
-    elif(p==2):
-        X = [1, x, y, x**2, x*y, y**2]
-        assert np.all(abs(X- generateDesignmatrix(p,x,y))<1e8)
+def test_design():
+    # TODO: make seperate for all p's??
+    for p in range(1,6):
+        if (p==1):
+            X = [np.ones(len(x)), x, y]
+            assert np.all(abs(convert(X)- generateDesignmatrix(p,x,y))<1e8)
 
-    elif(p==3):
-        X = [1, x, y, x**2, x*y, y**2, x**3, x**2*y, x*y**2, y**3]
-        assert np.all(abs(X- generateDesignmatrix(p,x,y))<1e8)
+        elif(p==2):
+            X = [np.ones(len(x)), x, y, x**2, x*y, y**2]
+            assert np.all(abs(convert(X)- generateDesignmatrix(p,x,y))<1e8)
 
-    elif(p==4):
-        X = [1, x, y, x**2, x*y, y**2, x**3, x**2*y, x*y**2, y**3, x**4, x**3*y, x**2*y**2, x*y**3, y**4]
-        assert np.all(abs(X- generateDesignmatrix(p,x,y))<1e8)
+        elif(p==3):
+            X = [np.ones(len(x)), x, y, x**2, x*y, y**2, x**3, x**2*y, x*y**2, y**3]
+            assert np.all(abs(convert(X)- generateDesignmatrix(p,x,y))<1e8)
 
-    elif(p==5):
-        X = [1, x, y, x**2, x*y, y**2, x**3, x**2*y, x*y**2, y**3, x**4, x**3*y, x**2*y**2, x*y**3, y**4,
-            x**5, y*x**4, x**3*y**2, x**2*y**3, x*y**4, y**5]
-        assert np.all(abs(X- generateDesignmatrix(p,x,y))<1e8)
+        elif(p==4):
+            X = [np.ones(len(x)), x, y, x**2, x*y, y**2, x**3, x**2*y, x*y**2, y**3, x**4, x**3*y, x**2*y**2, x*y**3, y**4]
+            assert np.all(abs(convert(X)- generateDesignmatrix(p,x,y))<1e8)
+
+        elif(p==5):
+            X = [np.ones(len(x)), x, y, x**2, x*y, y**2, x**3, x**2*y, x*y**2, y**3, x**4, x**3*y, x**2*y**2, x*y**3, y**4,
+                x**5, y*x**4, x**3*y**2, x**2*y**3, x*y**4, y**5]
+            assert np.all(abs(convert(X)- generateDesignmatrix(p,x,y))<1e8)
 
 def test_mse():
     assert abs(mean_squared_error(x,y) - model_selection_new.GridSearchNew.mean_squared_error(x, y)) < 1e-8
@@ -71,11 +79,3 @@ def test_ridge():
     scikit_ridge=linear_model.RidgeCV(alphas=[0.1], fit_intercept=False)
     scikit_ridge.fit(X, z)
     assert  np.all(abs( our_betas == scikit_ridge.coef_[:])<1e8)
-
-
-"""
-def test_train_test_split():
-    test that the indice subset are disjoint --> empty section. 
-
-    assert
-"""
