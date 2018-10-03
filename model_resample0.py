62379af3c9f840b8dd8d33270a8f7c53afcf6b19
Hanna Svennevik <svennevikh@gmail.com>
2018-10-03 08:22:28 +0200

    changed order first split then resample

--- model_resample0.py
+++ model_resample0.py
@@ -21,19 +21,20 @@ def model_resample(models, lmd, X, z, nboots, split_size = 0.2):
     r2_test = {"ridge": [], "lasso": [], "ols": []}
     z_pred_test = {"ridge": [], "lasso": [], "ols": []}  # mean av alle b0
     reg_coeffs = {}
-    "       ----------------------"
+
     mse_train = {"ridge": [], "lasso": [], "ols": []}
     r2_train = {"ridge": [], "lasso": [], "ols": []}
     z_pred_train = {"ridge": [], "lasso": [], "ols": []}  # mean av alle b0
 
+    # Splitting the data in order to test all of the resamples against the same test data.
+    X_train, X_test, z_train, z_test = train_test_split(
+        X, z, split_size=split_size
+    )
+
     for random_state in random_states:
 
-        # Resample and split data.
-        X_subset, z_subset = bootstrap(X, z, random_state)
-
-        X_train, X_test, z_train, z_test = train_test_split(
-            X_subset, z_subset, split_size=split_size
-        )
+        # Resample the training data.
+        X_, z_ = bootstrap(X_train, z_train, random_state)
 
         reg_coeffs[random_state] = {}
         for name, model in models.items():
@@ -41,19 +42,18 @@ def model_resample(models, lmd, X, z, nboots, split_size = 0.2):
             estimator = model(lmd[name])
             # Train a model for this pair of lambda and random state
             """  Keeping information for test """
-            estimator.fit(X_train, z_train)
-            temp = estimator.predict(X_test)
+            estimator.fit(X_, z_)
+            pred  = estimator.predict(X_test)
             reg_coeffs[random_state][name] = estimator.coef_
-            mse_test[name].append(mean_squared_error(z_test, temp))
-            r2_test[name].append(r2_score(z_test, temp))
-            z_pred_test[name].append(temp)
-
+            mse_test[name].append(mean_squared_error(z_test, pred))
+            r2_test[name].append(r2_score(z_test, pred))
+            z_pred_test[name].append(pred)
 
             """Lagre information about training """
-            temp = estimator.predict(X_train)
-            mse_train[name].append(mean_squared_error(z_train, temp))
-            r2_train[name].append(r2_score(z_train, temp))
-            z_pred_train[name].append(temp)
+            pred = estimator.predict(X_)
+            mse_train[name].append(mean_squared_error(z_train, pred))
+            r2_train[name].append(r2_score(z_train, pred))
+            z_pred_train[name].append(pred)
 
     """   Calulations done to get the information on correct format    """
     mse_avg_test = {"ridge": np.array(mse_test["ridge"]).mean(),"lasso": np.array(mse_test["lasso"]).mean(),"ols": np.array(mse_test["ols"]).mean() }
